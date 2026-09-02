ded2f9a2d1ef3b481b52f251c3edd28c5a65c420
letzbug <129280428+letzbug@users.noreply.github.com>
2026-09-02 08:42:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const VERSION = "v49";
+const VERSION = "v47";
 const CACHE_NAME="unipop-formateur-"+VERSION;
 const STATIC_ASSETS=[
-  "./","./index.html","./style.css?v=49","./app.js?v=49","./manifest.webmanifest?v=32",
+  "./","./index.html","./style.css?v=47","./app.js?v=47","./manifest.webmanifest?v=32",
   "./data/locations.json","./assets/icon.svg","./assets/luxembourg-skyline.png",
   "./assets/demo-map.jpg","./assets/demo-building.jpg","./assets/demo-entry.jpg","./assets/demo-room.jpg"
 ];
